f646e42c4b507f5a35b00cf424fa6b893bc4f36f
Luis Quintanilla <equalsgains@gmail.com>
2018-04-30 23:37:32 -0600

    created post route to successfully register

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/authDemo");
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "bop bop is the best!",
     resave: false,
@@ -43,6 +44,21 @@ app.get("/secret", function(req, res){
 // show sign up form
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.passport
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
 });
 
 
